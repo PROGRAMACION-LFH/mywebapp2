6d6bf8a8574dae071f0ffe9be7f4fc397b4eada4
Laura Fabian <21080120@upemor.edu.mx>
2022-10-13 00:56:09 -0500

    Actualizacion mywebapp 13oct2022

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -6,7 +6,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
-
 import os
 import sys
 
@@ -14,8 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.mysite.settings'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.mysite.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
